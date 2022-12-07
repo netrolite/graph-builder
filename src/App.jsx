dc1e90e6ab79dc807562bf1172dcdc7ed4d3fd90
Matvey <morozovmatvej75@gmail.com>
2022-12-07 10:00:58 +0300

    cool black square on canvas

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,36 @@ import { useEffect, useRef } from "react"
 
 export default function App() {
     const canvasRef = useRef(null);
-	useEffect(() => {
 
+	useEffect(() => {
+		resizeCanvas();
+		window.addEventListener("resize", resizeCanvas);
+		
+		// c === context
+		let c = canvasRef.current.getContext("2d");
+		fillRectCenter(400, 400);
+
+
+		
+		function fillRectCenter(x, y) {
+			const centerX = canvasRef.current.offsetWidth / 2 - x / 2;
+			const centerY = canvasRef.current.offsetHeight / 2 - y / 2;
+			c.fillRect(centerX, centerY, x, y);
+		}
+
+		function resizeCanvas() {
+			canvasRef.current.width = canvasRef.current.offsetWidth;
+			canvasRef.current.height = canvasRef.current.offsetHeight;
+		}
+		
+		return () => {
+			window.removeEventListener("resize", resizeCanvas);
+		}
 	}, [])
 	
     return (
-		<main className="container">
-			<div className="row">
-				<div className="col">
-					<canvas ref={canvasRef} />
-				</div>
-			</div>
-		</main>	
+		<main className="container min-vh-100 p-3">
+			<canvas ref={canvasRef} className="w-100 border border-primary rounded hgt-600" />
+		</main>
   	)
 }
