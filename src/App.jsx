70dbc38ba7ba0eebd4e7de1acfc4ac2df318eb14
Matvey <67281212+matvey733@users.noreply.github.com>
2022-12-09 13:01:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 import { createContext, useEffect, useRef } from "react"
 
 export default function App() {
-	const canvasRef = useRef(null);
-	
-	useEffect(() => {
+	function App() {
+    const canvasRef = React.useRef();
+
+    React.useEffect(() => {
 		window.addEventListener("resize", update);
-		/** @type {CanvasRenderingContext2D} */
-		let ctx = canvasRef.current.getContext("2d");
+      update();
 
-		const colorArray = [
-			'#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-			'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-			'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-			'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-			'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-			'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-			'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-			'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-			'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-			'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
-		];
+      window.setInterval(() => {
+          paint(canvasRef.current.getContext("2d"))
+      }, 10)
 		
+		return () => {
+			window.removeEventListener("resize", update);
+		}
+	}, [])
 
-		function paint() {
-			generateCircles(canvasRef.current, 100000, 20, ["darkblue", "green"], 1); 
+
+   function paint(ctx) {
+			generateCircles(canvasRef.current, 1, 200, ["black", "yellow"], 1); 
 
 			function generateCircles(canvas, amount, radius, colors, lineWidth) {
-				const startTime = new Date().getTime();
 				ctx.lineWidth = lineWidth;
 
 				for (let i = 0; i < amount; i++) {
@@ -42,23 +37,7 @@ export default function App() {
 					);
 					ctx.stroke();
 				}
-				console.log(new Date().getTime() - startTime)
 			}
-
-
-
-
-			// ctx.arc(60, 60, 50, 0, Math.PI * 2);
-			// ctx.fillStyle = "darkblue";
-			// ctx.fill();
-
-			// function animate() {
-			// 	requestAnimationFrame(animate);
-			// 	console.log("frame")
-			// }
-
-			// animate();
-
 		}
 
 		
@@ -72,20 +51,16 @@ export default function App() {
 
 		function update() {
 			resizeCanvas(canvasRef.current);
-			paint();
+			paint(canvasRef.current.getContext("2d"));
 		}
-		
-		update();
-		return () => {
-			window.removeEventListener("resize", update);
-		}
-	}, [])
-
-
 
     return (
-		<main className="container-fluid p-3">
-			<canvas ref={canvasRef} width="1880" height="920" className="border border-primary rounded" />
-		</main>
-  	)
+					   <main className="container-fluid p-3">
+            <canvas
+												    ref={canvasRef}
+																onClick={() => paint(canvasRef.current.getContext("2d"))};
+																className="border border-primary rounded"
+													/>
+								</main>
+    )
 }
