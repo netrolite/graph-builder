f281095aed78ce230b63809eb89b07a2d9e93a17
Matvey <morozovmatvej75@gmail.com>
2022-12-10 20:04:09 +0300

    Shape super class & Circle class

--- src/App.jsx
+++ src/App.jsx
@@ -17,31 +17,20 @@ export default function App() {
 	function draw() {
 		const canvas = canvasRef.current;
 		/** @type {CanvasRenderingContext2D} */ const c = canvas.getContext("2d");
+		c.strokeStyle = "red"
 
-		class Circle {
-			constructor(radius, velocity) {
-				this.x = Math.random() * (canvas.width - radius * 2) + radius;
-				this.y = Math.random() * (canvas.height - radius * 2) + radius;
+		// this is a super class for other classes. Do not call it directly
+		class Shape {
+			constructor(velocity) {
 				this.vx = (Math.random() - 0.5) * velocity;
 				this.vy = (Math.random() - 0.5) * velocity;
-				this.radius = radius;
-
-			}
-
-			draw() {
-				c.beginPath();
-				c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-				c.fillStyle = "purple";
-				c.fill();
-				c.strokeStyle = "blue"
-				c.lineWidth = 3;
-				c.stroke();
 			}
 
 			update() {
 				if (this.x < this.radius || this.x > canvas.width - this.radius) {
 					this.vx = -this.vx;
 				}
+
 				if (this.y < this.radius || this.y > canvas.height - this.radius) {
 					this.vy = -this.vy;
 				}
@@ -52,30 +41,44 @@ export default function App() {
 			}
 		}
 
+		class Circle extends Shape {
+			constructor(radius, ...args) {
+				super(...args);
+				this.x = Math.random() * (canvas.width - radius * 2) + radius;
+				this.y = Math.random() * (canvas.height - radius * 2) + radius;
+				this.radius = radius;
+			}
+
+
+			draw() {
+				c.beginPath();
+				c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+				c.fill();
+			}
+		}
+
 		if (!circles.length) {
-			for (let i = 0; i < 100; i++) {
-				circles.push(new Circle(1, 5))
+			for (let i = 0; i < 1; i++) {
+				circles.push(new Circle(20, 6));
 			}
 		}
 
 		function animate() {
 			requestAnimationFrame(animate);
 			c.clearRect(0, 0, canvas.width, canvas.height);
-			circles.forEach(item => {
-				item.update();
-			});
+			circles.forEach(item => item.update());
 		}
 		animate();
 	}
 
 	function resize() {
-		canvasRef.current.width = canvasRef.current.offsetWidth;
-		canvasRef.current.height = canvasRef.current.offsetHeight;
+		canvasRef.current.width = window.innerWidth;
+		canvasRef.current.height = window.innerHeight;
 	}
 
 	return (
-		<main className="container-fluid p-2">
-			<canvas ref={canvasRef} className="border border-2 border-primary rounded" />
+		<main className="container-fluid p-0">
+			<canvas ref={canvasRef} />
 		</main>
 	)
 }
