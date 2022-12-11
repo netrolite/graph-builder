80d8aa96fb744ed2acdfff3db294625407ef8fa6
Matvey <morozovmatvej75@gmail.com>
2022-12-11 18:33:31 +0300

    fix rectangles getting stuck inside canvas border

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ export default function App() {
 		class Shape {
 			constructor(colors, velocity) {
 				this.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+				this.lineWidth = 3;
 				
 				const randVx = Math.random() - 0.5;
 				const randVy = Math.random() - 0.5;
@@ -48,8 +49,78 @@ export default function App() {
 					this.vx = randVx >= 0 ? randVx * velocity + velocity : randVx * velocity - velocity;
 					this.vy = randVy * velocity;
 				}
-				this.initVx = this.vx;
-				this.initVy = this.vy;
+				this.initPositiveVx = Math.abs(this.vx);
+				this.initPositiveVy = Math.abs(this.vy);
+				this.initNegativeVx = -Math.abs(this.vx);
+				this.initNegativeVy = -Math.abs(this.vy);
+			}
+		}
+
+		class Rectangle extends Shape {
+			constructor(width, height, cornerRadius, ...args) {
+				super(...args);
+				this.x = Math.random() * (canvas.width - width);
+				this.y = Math.random() * (canvas.height - height);
+				this.width = width;
+				this.height = height;
+				this.minWidth = width;
+				this.minHeight = height;
+				this.maxWidth = width + 50;
+				this.maxHeight = height + 50;
+				this.cornerRadius = cornerRadius;
+			}
+
+			diff(mousePos, shapePos) {
+				return Math.abs(mousePos - shapePos);
+			}
+
+			draw() {
+				c.beginPath();
+				c.strokeStyle = this.strokeStyle;
+				c.lineWidth = this.lineWidth;
+				c.roundRect(this.x, this.y, this.width, this.height, this.cornerRadius);
+				c.stroke();
+			}
+			
+			update() {
+				// grow if cursor is within 100px range from the rectangle's center
+				if (
+					this.diff(mousePos.x, this.x + this.width / 2) <= 100
+					&& this.diff(mousePos.y, this.y + this.height / 2) <= 100
+					&& this.width <= this.maxWidth
+				) {
+					this.x -= 1;
+					this.y -= 1;
+					this.width += 2;
+					this.height += 2;
+				}
+				else if (this.width > this.minWidth) {
+					this.x += 1;
+					this.y += 1;
+					this.width -= 2;
+					this.height -= 2;
+				}
+
+
+				// if rectangle is outside of canvas dimensions, reverse its direction
+				// positive velocity turns to negative and vice versa
+				if (this.x < 0) {
+					this.vx = this.initPositiveVx;
+				}
+				else if (this.x > canvas.width - this.width) {
+					this.vx = this.initNegativeVx;
+				}
+
+				if (this.y < 0) {
+					this.vy = this.initPositiveVy;
+				}
+				else if (this.y > canvas.height - this.height) {
+					this.vy = this.initNegativeVy;
+				}
+
+				this.x += this.vx;
+				this.y += this.vy;
+				this.draw();
 			}
 		}
 
@@ -64,6 +135,7 @@ export default function App() {
 			draw() {
 				c.beginPath();
 				c.strokeStyle = this.strokeStyle;
+				c.lineWidth = this.lineWidth;
 				c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
 				c.stroke();
 			}
@@ -83,74 +155,11 @@ export default function App() {
 			}
 		}
 
-		class Rectangle extends Shape {
-			constructor(width, height, cornerRadius, ...args) {
-				super(...args);
-				this.x = Math.random() * (canvas.width - width);
-				this.y = Math.random() * (canvas.height - height);
-				this.width = width;
-				this.height = height;
-				this.minWidth = width;
-				this.minHeight = height;
-				this.maxWidth = width + 50;
-				this.maxHeight = height + 50;
-				this.cornerRadius = cornerRadius;
-			}
-
-			diff(mousePos, elemPos) {
-				return Math.abs(mousePos - elemPos);
-			}
-
-			draw() {
-				c.beginPath();
-				c.strokeStyle = this.strokeStyle;
-				c.roundRect(this.x, this.y, this.width, this.height, this.cornerRadius);
-				c.stroke();
-			}
-			
-			update() {
-				// grow if cursor is within 50px range
-				if (
-					this.diff(mousePos.x, this.x + this.width / 2) <= 100
-					&& this.diff(mousePos.y, this.y + this.height / 2) <= 100
-					&& this.width <= this.maxWidth
-				) {
-					console.log("grow");
-					this.x -= 1;
-					this.y -= 1;
-					this.width += 2;
-					this.height += 2;
-				}
-				else if (this.width > this.minWidth) {
-					console.log("shrink");
-					this.x += 1;
-					this.y += 1;
-					this.width -= 2;
-					this.height -= 2;
-				}
-
-				// BUG: this code does not accound for cases when this.initVx or this.initVy is negative
-				// if is further than canvas's dimensions, reverse direction
-				if (this.x < 0 || this.x > canvas.width - this.width) {
-					if (this.vx > 0 && this.initVx > 0) this.vx = -this.initVx;
-					else this.vx = this.initVx;
-				}
-				if (this.y < 0 || this.y > canvas.height - this.height) {
-					if (this.vy > 0) this.vy = -this.initVy;
-					else this.vy = this.initVy;
-				}
-
-				this.x += this.vx;
-				this.y += this.vy;
-				this.draw();
-			}
-		}
-
 		if (!shapes.length) {
-			for (let i = 0; i < 50; i++) {
-				// new Rectangle(width, height, borderRadius, velocity);
-				shapes.push(new Rectangle(40, 40, [5], ["darkblue"], 1))
-				// new Cirlce(radius, velocity);
+			for (let i = 0; i < 10; i++) {
+				// new Rectangle(width, height, borderRadius, colors, velocity);
+				shapes.push(new Rectangle(40, 40, [5], ["darkblue"], 8))
+				// new Cirlce(radius, colors, velocity);
 			}
 		}
 
