4c7cc0315b3f7d7a4df79b65d52f05c2798ca590
Matvey <morozovmatvej75@gmail.com>
2022-12-13 18:41:22 +0300

    new color pallete & redesigned input screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
 import { useState } from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Input from "./Input"
 import Animation from "./Animation"
 
 export default function App() {
 	const [animData, setAnimData] = useState({
 		amount: 10,
-		shapeTypes: ["circle", "rectangle"],
+		shapeTypes: [
+			{
+				type: "rectangle",
+				width: 80,
+				height: 50,
+				cornerRadius: 0,
+				filled: false,
+				borderColor: "#000"
+			},
+			{
+				type: "circle",
+				radius: 30,
+				filled: false,
+				borderColor: "#000"
+			}
+		],
 		velocity: 4,
-		fillColors: ["green", "darkblue"]
+		colors: ["#A1A2A6", "#024959", "#F2C12E", "#F2AE30", "#593E25"]
 	})
 
 	return (
-		<main className="container">
+		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Input setAnimData={setAnimData} />} />
-					<Route path="/animation" element={<Animation animData={animData} />} />
+					<Route
+						path="/"
+						element={
+							<Input
+								animData={animData}
+								setAnimData={setAnimData}
+							/>
+						} />
+					<Route
+						path="/animation"
+						element={
+							<Animation
+								animData={animData}
+							/>
+						}
+					/>
 				</Routes>
 			</BrowserRouter>
-		</main>
+		</>
 	)
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,19 +1,46 @@
 import { Link } from "react-router-dom"
 
-export default function Input(props) {
+export default function Input({setAnimData}) {
     return (
-        <div className="input-wrapper">
-        <label htmlFor="shapes-amount" className="form-label">Amount of shapes</label>
-        <div className="input-group mb-3">
-            <input type="number" id="shapes-mount"  className="form-control" />
-        </div>
-            
-            <input type="color" />
-            <input type="password" />
+        <div className="container mt-4">
+            <h1 className="mb-4 fw-semibold">Animation</h1>
 
-            <Link to="/animation">
-                <button type="submit">Start Animation</button>
-            </Link>
+            <label htmlFor="shapes-amount" className="form-label heading fw-semibold">Amount of Shapes</label>
+            <div className="input-group mb-4">
+                <input type="number" id="shapes-amount"  className="form-control" />
+            </div>
+
+            <p className="heading mb-2 fw-semibold">Types of Shapes</p>
+            <div className="d-flex align-items-center gap-2 mb-0">
+                <input
+                    className="input-checkbox"
+                    type="checkbox"
+                    checked=""
+                    id=""
+                />
+                <label htmlFor="Rectangles" className="text">Rectangles</label>
+            </div>
+
+            <div className="d-flex align-items-center gap-2 mb-3">
+                <input
+                    className="input-checkbox"
+                    type="checkbox"
+                    checked=""
+                    id=""
+                />
+                <label htmlFor="Rectangles" className="text">Circles</label>
+            </div>
+
+            <div className="mt-4">
+                <Link to="/animation">
+                    <button
+                        type="submit"
+                        className="btn w-100"
+                    >
+                        Start Animation
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
