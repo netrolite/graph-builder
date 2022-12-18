4aacdcb034eeb8b6b53e714077a75eccea6e3c0e
Matvey <morozovmatvej75@gmail.com>
2022-12-18 15:06:16 +0300

    change "Start Animation" button name

--- src/input/Input.jsx
+++ src/input/Input.jsx
@@ -3,23 +3,17 @@ import BrowseColorsPopup from "./components/popups/BrowseColorsPopup";
 import ShapesAmount from "./components/ShapesAmount";
 import Velocity from "./components/Velocity";
 import ShapeTypes from "./components/ShapeTypes";
-import Palettes from "./components/Palettes";
 import FillColors from "./components/FIllColors";
-import StartAnim from "./components/buttons/StartAnim";
+import StartAnimBtn from "./components/buttons/StartAnimBtn";
 import RectSettings from "./components/RectSettings";
 import CircSettings from "./components/CircSettings";
-import { useState, useContext } from "react";
-import { AnimDataContext, SetAnimDataContext } from "../App";
+import { useState } from "react";
 
 export default function Input() {
     const [showPalettesPopup, setShowPalettesPopup] = useState(false);
-    const animData = useContext(AnimDataContext);
-    const setAnimData = useContext(SetAnimDataContext);
 
     return (
         <div className="container p-4 main">
-            
-
             <h1 className="mb-4 fw-semibold">Animation</h1>
 
             <div className="inputs">
@@ -32,7 +26,7 @@ export default function Input() {
                 <CircSettings />
             </div>
 
-            <StartAnim />            
+            <StartAnimBtn />
         </div>
     )
 }
