247dd708ccf6a438629cb64dca0e21dce7cae630
Matvey <morozovmatvej75@gmail.com>
2022-12-24 19:27:56 +0300

    fix import

--- src/routes/graphs/Graphs.jsx
+++ src/routes/graphs/Graphs.jsx
@@ -1,5 +1,34 @@
-
+import { useEffect, useRef } from "react";
 
 export default function Graphs() {
+    const canvasRef = useRef();
+    let canvas;
+    /** @type {CanvasRenderingContext2D} */ let c;
+    
+    useEffect(() => {
+        canvas = canvasRef.current;
+        c = canvas.getContext("2d");
+        resize();
+        draw();
+    }, [])
 
+    function draw() {
+        c.beginPath();
+        c.lineWidth = 10;
+        c.lineCap = "round"
+        c.moveTo(100, 100);
+        c.lineTo(200, 300);
+        c.stroke();
+    }
+
+    function resize() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+
+    return (
+        <main className="container-fluid p-0">
+			<canvas ref={canvasRef}>Sorry, your browser isn't supported</canvas>
+		</main>
+    )
 }
